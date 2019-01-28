4812fca4db2856b06e2eb61e0cbb76a1c6b2e42e
Andreas Trütschel <andreas@truetschel.net>
2019-01-28 15:41:45 +0100

    Added GetCustomAttributes extension method

--- src/AI4E.Utils/CustomAttributeProviderExtension.cs
+++ src/AI4E.Utils/CustomAttributeProviderExtension.cs
@@ -5,22 +5,19 @@ namespace AI4E.Utils
 {
     public static class CustomAttributeProviderExtension
     {
-        public static bool IsDefined<TCustomAttribute>(this ICustomAttributeProvider attributeProvider) 
-            where TCustomAttribute : Attribute
-        {
-            if (attributeProvider == null)
-                throw new ArgumentNullException(nameof(attributeProvider));
-
-            return attributeProvider.IsDefined(typeof(TCustomAttribute), inherit: false);
-        }
-
-        public static bool IsDefined<TCustomAttribute>(this ICustomAttributeProvider attributeProvider, bool inherit) 
+        public static bool IsDefined<TCustomAttribute>(this ICustomAttributeProvider attributeProvider, bool inherit = false) 
             where TCustomAttribute : Attribute
         {
             if (attributeProvider == null)
                 throw new ArgumentNullException(nameof(attributeProvider));
 
             return attributeProvider.IsDefined(typeof(TCustomAttribute), inherit);
+        }
+
+        public static TCustomAttribute[] GetCustomAttributes<TCustomAttribute>(this ICustomAttributeProvider attributeProvider, bool inherit = false)
+            where TCustomAttribute : Attribute
+        {
+            return (TCustomAttribute[])attributeProvider.GetCustomAttributes(typeof(TCustomAttribute), inherit);
         }
     }
 }
